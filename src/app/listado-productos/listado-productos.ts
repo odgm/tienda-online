6a369dff2ee9c8c9a2ce8e40e7d88d69ee0b9b68
odgm <ordyelizkh@gmail.com>
2026-06-20 19:25:20 -0300

    generar los componentes

---
+++ src/app/listado-productos/listado-productos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-productos',
+  imports: [],
+  templateUrl: './listado-productos.html',
+  styleUrl: './listado-productos.css',
+})
+export class ListadoProductos {}
